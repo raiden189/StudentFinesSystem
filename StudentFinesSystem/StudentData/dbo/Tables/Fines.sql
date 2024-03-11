25ad4eed0c6a64eea131398b06c899bf8914fe5b
red <raidengreat123@gmail.com>
2024-03-11 12:17:30 +0800

    Register DB Tables
    tables for the basic register

---
+++ StudentFinesSystem/StudentData/dbo/Tables/Fines.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Fines]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Fine] MONEY NOT NULL DEFAULT 0, 
+    [Description] NVARCHAR(200) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [CreatedBy] NVARCHAR(50) NOT NULL
+)
---
+++ StudentFinesSystem/StudentData/dbo/Tables/Role.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Role]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [RoleKey] NVARCHAR(50) NOT NULL, 
+    [RoleValue] NVARCHAR(50) NOT NULL, 
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(),
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
